236e1edec78352a5ca6885f83b87116053b5354c
git config --global user.name Njugi <nmsteve45@gmail.com>
2021-10-19 08:28:06 +0300

    Added  Function to capture the input

--- index.js
+++ index.js
@@ -13,8 +13,33 @@ function EOQ(a,o,c) {
     
 }
 
-demand = 100
-orderingCost = 60.50
-carrringCost = 0.25
+//Getting input from html
+var inputDemand = document.getElementById("inputDemand")
+var inputOrderingCost = document.getElementById("inputOrderingCost")
+var inputCarryingCost = document.getElementById("inputCarryingCost")
 
-console.log(EOQ(demand,orderingCost,carrringCost))
+var results = document.getElementById("results")
+
+// Function to capture the input and pass it to the EOQ Function
+function calculate(Event){ 
+    
+    if (!inputDemand.value || !inputOrderingCost.value || !inputCarryingCost.value){
+        alert("Enter Some data")
+    }
+
+    else{
+    demand = parseFloat(inputDemand.value)
+    orderingCost = parseFloat(inputOrderingCost.value)
+    carrringCost = parseFloat(inputCarryingCost.value)
+    Event.preventDefault()
+    var result =Math.round(EOQ(demand,orderingCost,carrringCost))
+
+    console.log(result)
+    results.innerText = result
+
+    }
+}
+
+//action listener
+form.preventDefault
+form.addEventListener('submit',calculate )
